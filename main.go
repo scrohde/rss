8144955ac8cc7c89b74b838a90e7dbc6c59cf374
srohde <srohde@gmail.com>
2026-02-04 09:52:24 -0800

    Show compact relative time badge

--- main.go
+++ main.go
@@ -83,6 +83,7 @@ type ItemView struct {
 	Link             string
 	SummaryHTML      template.HTML
 	PublishedDisplay string
+	PublishedCompact string
 	IsRead           bool
 }
 
@@ -1499,8 +1500,10 @@ func scanItemView(rows *sql.Rows) (ItemView, error) {
 func buildItemView(id int64, title, link string, summary, content sql.NullString, published, readAt sql.NullTime) ItemView {
 	summaryHTML := pickSummaryHTML(summary, content)
 	publishedDisplay := "Unpublished"
+	publishedCompact := "na"
 	if published.Valid {
 		publishedDisplay = formatTime(published.Time)
+		publishedCompact = formatRelativeShort(published.Time, time.Now())
 	}
 	return ItemView{
 		ID:               id,
@@ -1508,6 +1511,7 @@ func buildItemView(id int64, title, link string, summary, content sql.NullString
 		Link:             link,
 		SummaryHTML:      summaryHTML,
 		PublishedDisplay: publishedDisplay,
+		PublishedCompact: publishedCompact,
 		IsRead:           readAt.Valid,
 	}
 }
@@ -1666,6 +1670,28 @@ func isDisallowedHost(host string) bool {
 
 func formatTime(t time.Time) string {
 	return t.Local().Format("Jan 2, 2006 - 3:04 PM")
+}
+
+func formatRelativeShort(t time.Time, now time.Time) string {
+	if t.IsZero() {
+		return "na"
+	}
+	age := now.Sub(t)
+	if age < 0 {
+		age = 0
+	}
+	switch {
+	case age < time.Minute:
+		return fmt.Sprintf("%ds", int(age.Seconds()))
+	case age < time.Hour:
+		return fmt.Sprintf("%dm", int(age.Minutes()))
+	case age < 24*time.Hour:
+		return fmt.Sprintf("%dh", int(age.Hours()))
+	case age < 365*24*time.Hour:
+		return fmt.Sprintf("%dd", int(age.Hours()/24))
+	default:
+		return fmt.Sprintf("%dy", int(age.Hours()/(24*365)))
+	}
 }
 
 func toggleRead(db *sql.DB, itemID int64) error {
